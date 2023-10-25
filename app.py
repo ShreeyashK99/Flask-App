5480f57815900240854ecd19cb4c436848ca63ed
Shreeyashk15 <132074520+Shreeyashk15@users.noreply.github.com>
2023-10-25 12:11:56 +0530

    First commit

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from flask import jsonify
 
 app = Flask(__name__)
-app.secret_key = "supersecretkey" 
+app.secret_key = "supersecretkey"
 
 
 app.config["MONGO_URI"] = "mongodb://localhost:27017/flask_signup_login_db"
 mongo = PyMongo(app)
 
 
-ADMIN_USERNAME = "admin"
-ADMIN_PASSWORD = "adminpassword"
+ADMIN_USERNAME = "Shreek"
+ADMIN_PASSWORD = "sh12345"
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -26,11 +28,10 @@ def signup():
             'fullname': request.form['fullname'],
             'email': request.form['email'],
             'username': request.form['username'],
-            'password': request.form['password'] 
+            'password': request.form['password']
         })
-        
-        flash('Successfully signed up!')
-        return redirect(url_for('login'))
+
+        return render_template('success_signup.html')
 
     return render_template('signup.html')
 
@@ -41,12 +42,9 @@ def login():
         login_user = users.find_one({'username': request.form['username']})
 
         if login_user and login_user['password'] == request.form['password']:
-            flash('Logged in successfully!')
-            return redirect(url_for('login'))
-            
-        flash('Invalid credentials!')
-        return redirect(url_for('login'))
+            return render_template('user_dashboard.html', username=request.form['username'])
 
+        flash('Invalid credentials!')
     return render_template('login.html')
 
 @app.route('/admin/login', methods=['GET', 'POST'])
@@ -56,11 +54,9 @@ def admin_login():
         password = request.form['password']
 
         if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
-            flash('Admin logged in successfully!')
             return redirect(url_for('admin_dashboard'))
 
         flash('Invalid admin credentials!')
-        return redirect(url_for('admin_login'))
 
     return render_template('admin_login.html')
 
@@ -69,19 +65,50 @@ def admin_dashboard():
     users = mongo.db.users.find()
     return render_template('admin_dashboard.html', users=users)
 
+@app.route('/users')
+def list_users():
+    users = mongo.db.users.find()
+    return render_template('list_users.html', users=users)
+
+@app.route('/users/update/<user_id>', methods=['GET', 'POST'])
+def update_user(user_id):
+    user = mongo.db.users.find_one_or_404({"_id": ObjectId(user_id)})
+    if request.method == 'POST':
+        mongo.db.users.update_one(
+            {"_id": ObjectId(user_id)},
+            {
+                "$set": {
+                    'fullname': request.form['fullname'],
+                    'email': request.form['email'],
+                    'username': request.form['username'],
+                    'password': request.form['password']
+                }
+            }
+        )
+        flash('User updated successfully!')
+        return redirect(url_for('list_users'))
+    return render_template('edit_user.html', user=user)
+
+@app.route('/users/delete/<user_id>')
+def delete_user(user_id):
+    mongo.db.users.delete_one({"_id": ObjectId(user_id)})
+    flash('User deleted successfully!')
+    return redirect(url_for('list_users'))
+
 @app.route('/api/users', methods=['GET'])
 def get_all_users():
     users = mongo.db.users.find()
-    user_data = []
-    
+    user_list = []
     for user in users:
-        user_data.append({
+        user_data = {
+            'id': str(user['_id']),
             'fullname': user['fullname'],
             'email': user['email'],
             'username': user['username']
-        })
-        
-    return jsonify(user_data)
+        }
+        user_list.append(user_data)
+    
+    return jsonify(user_list)
 
 if __name__ == '__main__':
     app.run(debug=True)
